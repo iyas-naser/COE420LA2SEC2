5a22e78b7b146eeb491b39259f4329ca54ad6f6f
iyas-naser <iyasnaser24@gmail.com>
2019-02-10 16:32:30 +0400

    Initial commit

---
+++ src/coe420la2sec2/COE420LA2SEC2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coe420la2sec2;
+
+/**
+ *
+ * @author b00071189
+ */
+public class COE420LA2SEC2 {
+    
+    public static void addFeatureA(){
+        System.out.println("Feature A added");
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
